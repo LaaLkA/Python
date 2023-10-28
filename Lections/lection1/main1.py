70d7adad1bf2945bba5376cb019e0da7ca200174
NikitaPCJulia <yulenka_ufa01@mail.com>
2023-10-28 12:10:24 +0300

    check it

---
+++ Lections/lection1/main1.py
@@ -0,0 +1,12 @@
+print("Введите первое число: ")
+a = input()
+b = input("Введите второе число: ")
+
+print (a, "+", b, "=", a + b) #Сложились не числа, а две строки Вывело - 34
+
+c = 5,89 
+print(c)
+print(type(c))
+c = int(c)
+print(type(c))
+print(c)
