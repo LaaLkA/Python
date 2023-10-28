627a6587a501a2640560ee34162fd232f353a6a2
Афонченков Н.А. <AfonchenkovNA@p4udp.local>
2023-10-28 13:59:52 +0300

    Completed lection1

--- Lections/lection1/main1.py
+++ Lections/lection1/main1.py
@@ -1,12 +1,32 @@
+# print("Введите первое число: ")
+# a = input()
+# b = input("Введите второе число: ")
+
+# print (a, "+", b, "=", a + b) #Сложились не числа, а две строки Вывело - 34
+
+# c = 5.9
+# print(type(c))
+# print(c)
+# c = int(c)
+# print(type(c))
+# print(c)
+
+# c = 5.9
+# print(type(c))
+# print(c)
+# c = str(c)
+# print(type(c))
+# print(c + "89")
+
+# c = 1
+# print(type(c))
+# print(c)
+# c = bool(c)
+# print(type(c))
+# print(c)
+
 print("Введите первое число: ")
-a = input()
-b = input("Введите второе число: ")
+a = int(input())
+b = int(input("Введите второе число: "))
 
-print (a, "+", b, "=", a + b) #Сложились не числа, а две строки Вывело - 34
-
-c = 5,89 
-print(c)
-print(type(c))
-c = int(c)
-print(type(c))
-print(c)
+print (a, "+", b, "=", a + b) 
