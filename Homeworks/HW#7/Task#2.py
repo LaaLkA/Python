bb0bc5b96104adbb173af37c1c67a67e5fc0a32f
Афонченков Н.А. <AfonchenkovNA@p4udp.local>
2023-12-26 16:52:24 +0300

    Completed Finaly test

--- Homeworks/HW#7/Task#2.py
+++ Homeworks/HW#7/Task#2.py
@@ -33,15 +33,24 @@ print_operation_table(lambda x, y: x * y, 3, 3)
 #             print(' '.join(row))
 
 
-def print_operation_table(operation, num_rows, num_columns):
+def print_operation_table(operation, num_rows=9, num_columns=9):
     if num_rows < 2 or num_columns < 2:
         print('ОШИБКА! Размерности таблицы должны быть больше 2!')
-    else:
-        header = ' '.join([str(i) for i in range(1, num_columns + 1)])
-        print(header)
-        for i in range(2, num_rows + 1):
-            row = [str(i)] + [str(operation(i, j)) for j in range(2, num_columns + 1)]
-            print(' '.join(row))
+        return
+    
+    for i in range(1, num_columns + 1):
+        print(i, end='')
+        if i < num_columns:
+            print('', end=' ')
+    print()
+        
+    for i in range(2, num_rows + 1):
+        print(i, end=' ')
+        for j in range(2, num_columns + 1):
+            print(operation(i,j), end='')
+            if j < num_columns:
+                print('', end=' ')
+        print()
 
 
-print_operation_table(lambda x, y: x * y, 3, 3)
+print_operation_table(lambda x, y: x + y, 4, 4)
